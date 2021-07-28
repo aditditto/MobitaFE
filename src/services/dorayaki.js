5cb1c39b294c79fa1bd2cfe607b461b0a5f717a1
Aditya Bimawan <aditditto@gmail.com>
2021-07-29 00:02:54 +0700

    add dorayaki service

---
+++ src/services/dorayaki.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiURL = "http://localhost:5000";
+
+export const getAllDorayaki = async () => {
+  const res = await axios.get(apiURL + "/dorayaki");
+  return res.data;
+};
+
+export const getOneDorayaki = async (_id) => {
+  const res = await axios.get(`${apiURL}/dorayaki/${_id}`);
+  return res.data;
+};
+
+export const newDorayaki = async (flavor, desc, img) => {
+  const res = await axios.post(`${apiURL}/dorayaki`, {
+    flavor: flavor,
+    description: desc,
+  });
+  return [res.status, res.headers.location];
+};
+
+export const deleteDorayaki = async (_id) => {
+  const res = await axios.delete(`${apiURL}/dorayaki/${_id}`);
+  return res.status;
+};
